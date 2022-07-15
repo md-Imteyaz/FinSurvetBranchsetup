9540b271d0f6f8c44580b310778e11f82f142cb5
md-Imteyaz <98022042+md-Imteyaz@users.noreply.github.com>
2022-07-15 11:26:17 +0530

    Add files via upload

---
+++ FinancialInstitutionInformationFinSurveyWebpage2Application.java
@@ -0,0 +1,13 @@
+package com.financialinstitution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialInstitutionInformationFinSurveyWebpage2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialInstitutionInformationFinSurveyWebpage2Application.class, args);
+	}
+
+}
